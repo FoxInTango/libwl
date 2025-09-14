8ba48c58a980147766fcb27a437bb80024e631b0
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-15 07:35:34 +0800

    Mon Sep 15 07:35:34 AM CST 2025

--- .make/depend.dir/libevent.mk
+++ .make/depend.dir/libevent.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libevent.recursive
 ECHO_TARGETS += libevent.echo
 libevent.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libwl/               >> /home/lidali/alpine/libraries/libevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libwl/.make >> /home/lidali/alpine/libraries/libevent/.make/super
-	cd /home/lidali/alpine/libraries/libevent/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libwl/               >> /volumes/llama/home/alpine/libraries/libevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libwl/.make >> /volumes/llama/home/alpine/libraries/libevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libevent/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libevent/.make/super
 libevent.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libwl/               >> /home/lidali/alpine/libraries/libevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libwl/.make >> /home/lidali/alpine/libraries/libevent/.make/super
-	cd /home/lidali/alpine/libraries/libevent/ && make echo
-	-rm /home/lidali/alpine/libraries/libevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libwl/               >> /volumes/llama/home/alpine/libraries/libevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libwl/.make >> /volumes/llama/home/alpine/libraries/libevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libevent/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libevent/.make/super
--- .make/depend.dir/libioevent.mk
+++ .make/depend.dir/libioevent.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libioevent.recursive
 ECHO_TARGETS += libioevent.echo
 libioevent.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libwl/               >> /home/lidali/alpine/libraries/libioevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libwl/.make >> /home/lidali/alpine/libraries/libioevent/.make/super
-	cd /home/lidali/alpine/libraries/libioevent/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libioevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libwl/               >> /volumes/llama/home/alpine/libraries/libioevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libwl/.make >> /volumes/llama/home/alpine/libraries/libioevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libioevent/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libioevent/.make/super
 libioevent.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libwl/               >> /home/lidali/alpine/libraries/libioevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libwl/.make >> /home/lidali/alpine/libraries/libioevent/.make/super
-	cd /home/lidali/alpine/libraries/libioevent/ && make echo
-	-rm /home/lidali/alpine/libraries/libioevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libwl/               >> /volumes/llama/home/alpine/libraries/libioevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libwl/.make >> /volumes/llama/home/alpine/libraries/libioevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libioevent/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libioevent/.make/super
