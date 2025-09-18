4729899452a1fcfc95ffe93faa8d55068533e780
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-19 00:40:45 +0800

    Fri Sep 19 12:40:45 AM CST 2025

--- .make/depend.dir/libfsevent.mk
+++ .make/depend.dir/libfsevent.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libfsevent.recursive
 ECHO_TARGETS += libfsevent.echo
 libfsevent.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libwl/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libwl/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libwl/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libwl/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
 libfsevent.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libwl/               >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libwl/.make >> /home/lidali/alpine/libraries/libfsevent/.make/super
-	cd /home/lidali/alpine/libraries/libfsevent/ && make echo
-	-rm /home/lidali/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libwl/               >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libwl/.make >> /volumes/llama/home/alpine/libraries/libfsevent/.make/super
+	cd /volumes/llama/home/alpine/libraries/libfsevent/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libfsevent/.make/super
