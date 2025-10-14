30ad76f6863d5aac694b6d40ea4ae273c5b9c0fe
李诗军 <lidali@AllinoneBook.local>
2025-10-14 15:57:57 +0800

    2025年10月14日 星期二 15时57分57秒 CST

--- .make/depend.dir/libioevent.mk
+++ .make/depend.dir/libioevent.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libioevent.recursive
 ECHO_TARGETS += libioevent.echo
 libioevent.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libwl/               >> /volumes/llama/home/alpine/libraries/libioevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libwl/.make >> /volumes/llama/home/alpine/libraries/libioevent/.make/super
-	cd /volumes/llama/home/alpine/libraries/libioevent/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libioevent/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libwl/               >> /Users/lidali/alpine/libraries/libioevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libwl/.make >> /Users/lidali/alpine/libraries/libioevent/.make/super
+	cd /Users/lidali/alpine/libraries/libioevent/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libioevent/.make/super
 libioevent.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libwl/               >> /volumes/llama/home/alpine/libraries/libioevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libwl/.make >> /volumes/llama/home/alpine/libraries/libioevent/.make/super
-	cd /volumes/llama/home/alpine/libraries/libioevent/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libioevent/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libwl/               >> /Users/lidali/alpine/libraries/libioevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libwl/.make >> /Users/lidali/alpine/libraries/libioevent/.make/super
+	cd /Users/lidali/alpine/libraries/libioevent/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libioevent/.make/super
